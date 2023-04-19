c02da35dd9f9e430ca5c4a68fc9889b5ad208a54
Valdir Fonseca <valdirkleiton@gmail.com>
2023-04-19 09:58:59 +0100

    Added questions, edit app.js and index.js

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const questions = require('./questions');
+const logger = require('./logger');
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json())
+app.use(logger);
+
+app.get('/', (req, res) => {
+    res.send(`Welcome to the QUIZ API! There are 4 categories available, each containig 10 questions.`);
+})
+
+
+app.get('/questions', (req, res) => {
+    res.send(questions);
+})
