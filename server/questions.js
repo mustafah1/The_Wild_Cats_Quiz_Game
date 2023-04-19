090de0cdf44a4d0d0df52516168f1a1f3fea9866
Valdir Fonseca <valdirkleiton@gmail.com>
2023-04-19 15:27:17 +0100

    questions file updated to nested array

---
+++ server/questions.js
@@ -0,0 +1,44 @@
+const questions = [
+  [
+    //First category
+    {
+      "When did WWII start?": ["1993", "1844", "1939", 2],
+    },
+    {
+      "Where was Chirchhill Born": ["London", "Oxford", "Leeds", 0],
+    },
+  ],
+
+  [
+    //Second Category
+    {
+      "Hello1": ["world", 2],
+    },
+    {
+      "Where is china located": ["asia", "africa", "europe", 0],
+    },
+  ],
+
+  [
+    //category one
+    {
+      "When did WWII start?": ["1993", "1844", "1939", 2],
+    },
+    {
+      "Where was Chirchhill Born": ["London", "Oxford", "Leeds", 0],
+    },
+  ],
+
+  [
+    //category one
+    {
+      "When did WWII start?": ["1993", "1844", "1939", 2],
+    },
+    {
+      "Where was Chirchhill Born": ["London", "Oxford", "Leeds", 0],
+    },
+  ],
+];
+
+module.exports = questions;
+
